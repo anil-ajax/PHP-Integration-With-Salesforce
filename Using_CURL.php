d0f42d93354dfbc4c893736c778ca868128db11b
Anil Kumar <anil.ajax@gmail.com>
2018-11-06 11:27:01 +0530

    PHP and Salesforce Integration using CURL

---
+++ Using_CURL.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * PHP and Salesforce Integration using CURL
+ * @author Anil Kumar (https://github.com/anil-ajax)
+ */
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://login.salesforce.com/services/oauth2/token");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            
+
+"grant_type=password&client_id=<client id from salesforce>&client_secret=<client sec=cret from salesforce>&username=<your salesforce usename>&password=<your salesforce password>");
+
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+//echo '<pre>';
+$res = json_decode($server_output);
+
+$token = $res->access_token;
+
+
+// get products 
+
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://anil99-dev-ed.my.salesforce.com/services/apexrest/productServiceV2");
+curl_setopt($ch, CURLOPT_POST, 0);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+     'Authorization: Bearer $token'
+    ));
+
+
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+echo '<pre>';
+$res = 'here'.$server_output;
+
+echo '<pre>';
+print_r($res);
+
+
+?>
